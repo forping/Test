52479dc86b236cafc67b447516edd1b9bf2b4eee
forping <forping@forpingdeMacBook-Pro.local>
2017-09-11 17:41:47 +0800

    xxx

---
+++ Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by forping on 2017/9/11.
+//  Copyright © 2017年 ylx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by forping on 2017/9/11.
+//  Copyright © 2017年 ylx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
